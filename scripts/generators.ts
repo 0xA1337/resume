4ecaaf2330aa9c381ca9cc531c5fc1427a8787a1
0xA1337 <0xa1337@proton.me>
2025-04-03 18:23:13 +0900

    fix: use no sandbox parameters for puppeteers

--- scripts/generators.ts
+++ scripts/generators.ts
@@ -1,7 +1,7 @@
-import fs from 'fs';
-import puppeteer, { MediaType } from 'puppeteer';
-import resumeJson from '../resume.json';
-import { render, pdfRenderOptions } from '..';
+import fs from "fs";
+import puppeteer, { MediaType } from "puppeteer";
+import { pdfRenderOptions, render } from "..";
+import resumeJson from "../resume.json";
 
 const asyncStreamWrite = (path: string, content: string) => {
   const stream = fs.createWriteStream(path);
@@ -26,14 +26,16 @@ export const generateHtml = async (path: string) => {
 export const generatePdf = async (path: string) => {
   const html = render(resumeJson);
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
 
-  await page.setContent(html, { waitUntil: 'networkidle0' });
+  await page.setContent(html, { waitUntil: "networkidle0" });
   await page.emulateMediaType(pdfRenderOptions.mediaType as MediaType);
   const pdf = await page.pdf({
     path,
-    format: 'Letter',
+    format: "Letter",
     printBackground: true,
     margin: pdfRenderOptions.margin,
   });
