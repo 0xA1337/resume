3be6622775de62e1b25b7a76fb9a394b4db4de88
0xA1337 <0xa1337@proton.me>
2025-04-03 18:25:20 +0900

    style: standardize quotation marks and update PDF filename in build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,16 +1,16 @@
-import { generateHtml, generatePdf } from './generators';
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
+import { generateHtml, generatePdf } from "./generators";
 
-const PUBLIC_FOLDER = path.join(__dirname, '..', 'public');
+const PUBLIC_FOLDER = path.join(__dirname, "..", "public");
 
 const main = async () => {
   if (!fs.existsSync(PUBLIC_FOLDER)) {
     fs.mkdirSync(PUBLIC_FOLDER);
   }
 
-  await generateHtml(path.join(PUBLIC_FOLDER, 'index.html'));
-  await generatePdf(path.join(PUBLIC_FOLDER, 'index.pdf'));
+  await generateHtml(path.join(PUBLIC_FOLDER, "index.html"));
+  await generatePdf(path.join(PUBLIC_FOLDER, "resume.pdf"));
 };
 
 main();
